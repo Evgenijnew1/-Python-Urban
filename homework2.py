defffb5582937e429af9eed51b37341a298787c6
Evgenijnew1 <Evgenij-bogomazov@mail.ru>
2024-06-11 17:43:47 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+make_task = 12
+hours_numb = 1.5
+first_name = "Piton"
+time_to_one = (hours_numb/make_task)
+print('курс: '+first_name+','+'всего задач'':',make_task,',''затрачено часов'':',hours_numb,',''среднее время выполнения',
+time_to_one, 'часа')
+
+
+
+
+
+
