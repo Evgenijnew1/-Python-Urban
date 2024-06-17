aa0982953858d04e8327b540af859077a78db233
Evgenijnew1 <Evgenij-bogomazov@mail.ru>
2024-06-17 16:51:07 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+tuple_ = 5 , 8 , 'g' , 'e'
+immutable_var = tuple_
+print('immutable_var:',immutable_var)
+#immutable_var[0] = 10
+#print(immutable_var)
+mutable_list = [4,8,10,'a','b']
+mutable_list[0] = 'for'
+mutable_list.append('biggest')
+mutable_list.extend(['Pulp Fiction' ])
+print('mutable_list:' ,mutable_list)
