c56a2d3404cf200aaac2c4e33a1b9d4e099f31d3
Evgenijnew1 <Evgenij-bogomazov@mail.ru>
2025-01-22 23:05:58 +0300

    Add files via upload

---
+++ module 3.5.py
@@ -0,0 +1,14 @@
+
+def get_multiplied_digits (number):
+    str_number= str(number)  # строковое значение чтобы можно было применить оператор [] для доступа по номеру индекса
+    first = int(str_number[0])
+    if len(str_number)>1:
+     return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+     return first
+result1 = get_multiplied_digits(int(str(40203) + str(1)))
+print(result1)
+result2 = get_multiplied_digits(int(str(402030) + str(1)))
+print(result2)
+
+
